2da39fb09cb1aeeee09e4250990c2514fa11f2c2
Dustin Gelegonya <187451552+dustingelegonya@users.noreply.github.com>
2025-12-14 22:29:15 -0500

    orphanedwindowninja 4.0.2

---
+++ Casks/orphanedwindowninja.rb
@@ -0,0 +1,12 @@
+cask "orphanedwindowninja" do
+  version "4.0.2"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/dustingelegonya/OrphanedWindowNinja/releases/download/v#{version}/OrphanedWindowNinja.zip"
+  name "OrphanedWindowNinja"
+  desc "macOS utility"
+  homepage "https://github.com/dustingelegonya/OrphanedWindowNinja"
+  app "OrphanedWindowNinja.app"
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/OrphanedWindowNinja.app"], sudo: false
+  end
+end
