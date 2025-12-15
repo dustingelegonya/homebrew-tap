473a21a3ef0591054c7c47ba0b54e74f55c891d5
Dustin Gelegonya <187451552+dustingelegonya@users.noreply.github.com>
2025-12-14 23:30:05 -0500

    orphanedwindowninja 1.0.0

--- Casks/orphanedwindowninja.rb
+++ Casks/orphanedwindowninja.rb
@@ -1,5 +1,5 @@
 cask "orphanedwindowninja" do
-  version "4.0.2"
+  version "1.0.0"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   url "https://github.com/dustingelegonya/OrphanedWindowNinja/releases/download/v#{version}/OrphanedWindowNinja.zip"
   name "OrphanedWindowNinja"
